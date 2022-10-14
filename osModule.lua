6cc135fd7297664b7e18ee0fedc019e806b3fc95
HappySunChild <90355743+HappySunChild@users.noreply.github.com>
2022-10-14 17:55:26 -0400

    Update osModule.lua

--- osModule.lua
+++ osModule.lua
@@ -3,25 +3,6 @@ local mod = {}
 
 local w, h = term.getSize()
 
-function mod.install()
-    fs.makeDir("os")
-    fs.makeDir("os/programs")
-
-    local programs = {
-        ["os/programs/boot"] = "https://raw.githubusercontent.com/HappySunChild/ComputerCraftPrograms/main/boot.lua",
-        ["os/programs/main"] = "https://raw.githubusercontent.com/HappySunChild/ComputerCraftPrograms/main/main.lua",
-        ["startup.lua"] = "https://raw.githubusercontent.com/HappySunChild/ComputerCraftPrograms/main/startup.lua"
-    }
-
-    for program, url in pairs(programs) do
-        if fs.exists(program) then
-            fs.delete(program)
-        end
-
-        shell.run(string.format("wget %s %s", url, program))
-    end
-end
-
 function mod.clear()
     term.clear()
     term.setCursorPos(1, 1)
