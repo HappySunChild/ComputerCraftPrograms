836bb8e01b1082dd4b702999ad9fef6a7f791ee9
HappySunChild <90355743+HappySunChild@users.noreply.github.com>
2022-10-15 14:19:51 -0400

    Update disco.lua

--- disco.lua
+++ disco.lua
@@ -17,12 +17,19 @@ local floorStart = { x = 273, y = 59, z = -176 }
 
 local function placeLamp(x, y, z, color)
     local lamp = string.format(lampBase, color)
-    commands.exec(string.format("setblock %s %s %s %s", x, y, z, lamp))
+    commands.execAsync(string.format("setblock %s %s %s %s", x, y, z, lamp))
 end
 
-for x = floorStart.x, floorStart.x + floorSize.x do
-    for z = floorStart.z, floorStart.z + floorSize.z do
-        local color = lamps[math.random(1, #lamps)]
-        placeLamp(x, floorStart.y, z, color)
+local function updateFloor()
+    for x = floorStart.x, floorStart.x + floorSize.x do
+        for z = floorStart.z, floorStart.z + floorSize.z do
+            local color = lamps[math.random(1, #lamps)]
+
+            placeLamp(x, floorStart.y, z, color)
+        end
     end
 end
+
+updateFloor()
+sleep(1)
+updateFloor()
