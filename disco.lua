1ccea17a1101f5dc4d117536039e0bda5dce163d
HappySunChild <90355743+HappySunChild@users.noreply.github.com>
2022-10-15 14:14:18 -0400

    Update disco.lua

--- disco.lua
+++ disco.lua
@@ -12,8 +12,8 @@ local lamps = {
 }
 
 local lampBase = "projectred-illumination:%s_illumar_lamp"
-local floorSize = { x = 7, z = -9 }
-local floorStart = { x = 273, y = 59, z = -167 }
+local floorSize = { x = 7, z = 9 }
+local floorStart = { x = 273, y = 59, z = -176 }
 
 local function placeLamp(x, y, z, color)
     local lamp = string.format(lampBase, color)
@@ -22,6 +22,7 @@ end
 
 for x = floorStart.x, floorStart.x + floorSize.x do
     for z = floorStart.z, floorStart.z + floorSize.z do
-        placeLamp(x, floorStart.y, z)
+        local color = lamps[math.random(1, #lamps)]
+        placeLamp(x, floorStart.y, z, color)
     end
 end
