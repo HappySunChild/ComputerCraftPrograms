c9d60943e82d7d257af8e1e15c0b63f4cde459d2
HappySunChild <90355743+HappySunChild@users.noreply.github.com>
2022-10-16 14:40:11 -0400

    Update disco.lua

--- disco.lua
+++ disco.lua
@@ -1,7 +1,7 @@
 local lamps = {
     "red",
     "orange",
-    "purple",
+    "yellow",
     "blue"
 }
 
@@ -9,30 +9,32 @@ local lampBase = "projectred-illumination:%s_illumar_lamp"
 local floorSize = { x = 6, z = 8 }
 local floorStart = { x = 273, y = 59, z = -176 }
 
-local function placeLamp(x, y, z, color)
+local function placeLamp(x, y, z, color, lit)
     local lamp = string.format(lampBase, color)
-    commands.execAsync(string.format("setblock %s %s %s %s[lit = true]", x, y, z, lamp))
+    commands.execAsync(string.format("setblock %s %s %s %s[lit = %s]", x, y, z, lamp, tostring(lit)))
 end
 
-local function updateFloor()
+local function updateFloor(lit)
     for x = floorStart.x, floorStart.x + floorSize.x do
         for z = floorStart.z, floorStart.z + floorSize.z do
             local color = lamps[math.random(1, #lamps)]
 
-            placeLamp(x, floorStart.y, z, color)
+            placeLamp(x, floorStart.y, z, color, lit)
         end
     end
 end
 
 local function disco()
     while true do
-        updateFloor()
-        sleep(1)
+        updateFloor(true)
+        sleep(0.4)
     end
 end
 
 local function music()
     shell.run("player play dfpwm JELLY 4")
+
+    updateFloor(false)
 end
 
 parallel.waitForAny(disco, music)
