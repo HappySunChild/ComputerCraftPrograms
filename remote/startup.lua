6acd171afa9d83e3f4e7a4bb51a32853d8903931
HappySunChild <90355743+HappySunChild@users.noreply.github.com>
2022-10-20 10:00:38 -0400

    Update startup.lua

--- remote/startup.lua
+++ remote/startup.lua
@@ -1,4 +1,8 @@
--- this is the one you should download because it will download the main program and will automatically run it
+-- probably will go into a disk
+
+if disk.isPresent("top") then
+    fs.copy("disk/startup.lua", "startup.lua")
+end
 
 if not fs.exists("reciever.lua") then
     shell.run("wget https://raw.githubusercontent.com/HappySunChild/ComputerCraftPrograms/main/remote/main.lua reciever.lua")
