59bd66b4ba2374af8208a05d3137930d20267051
name <90355743+HappySunChild@users.noreply.github.com>
2023-09-10 22:03:48 -0400

    Update turtlemain.lua

--- turtleremote/turtlemain.lua
+++ turtleremote/turtlemain.lua
@@ -15,13 +15,22 @@ local programs = {
 	["tunnel.lua"] = "https://raw.githubusercontent.com/HappySunChild/ComputerCraftPrograms/main/turtleremote/programs/tunnel.lua",
 }
 
+local function broadcastStatus(status)
+	local currentFuel = turtle.getFuelLevel()
+	local fuelLimit = turtle.getFuelLimit()
+	
+	local dataString = string.format("%s\\%s\\%d\\%d", os.getComputerLabel(), status, currentFuel, fuelLimit)
+	
+	rednet.broadcast(dataString, "tStatus")
+end
+
 local function runProgram(program, args)
 	local path = fs.combine("programs", program)
 	
 	if (fs.exists(path)) then
-		shell.run(path, table.unpack(args))
+		shell.run(path, table.unpack(args or {}))
 	else
-		shell.run(path, table.unpack(args))
+		shell.run(program, table.unpack(args or {}))
 	end
 end
 
@@ -39,28 +48,117 @@ local function downloadPrograms(replace)
 	end
 end
 
+local handlers = {
+	forward = function ()
+		repeat
+			turtle.dig()
+		until (turtle.forward())
+	end,
+	back = function ()
+		if (not turtle.back()) then
+			turtle.turnRight()
+			turtle.turnRight()
+			
+			repeat
+				turtle.dig()
+			until (turtle.forward())
+			
+			turtle.turnLeft()
+			turtle.turnLeft()
+		end
+	end,
+	up = function ()
+		repeat
+			turtle.digUp()
+		until (turtle.up())
+	end,
+	down = function ()
+		repeat
+			turtle.digDown()
+		until (turtle.down())
+	end,
+	left = turtle.turnLeft,
+	right = turtle.turnRight
+}
+
 local actions = {
 	miner = function (args)
-		print("running miner.lua", table.concat(args))
+		broadcastStatus("Mining")
 		
 		runProgram("miner.lua", args)
 	end,
 	tunnel = function (args)
+		broadcastStatus("Tunneling")
+		
 		runProgram("tunnel.lua", args)
 	end,
 	bridge = function (args)
+		broadcastStatus("Bridging")
+		
 		runProgram("bridge.lua", args)
 	end,
 	refuel = function (args)
+		broadcastStatus("Refueling")
+		
 		runProgram("refuel.lua", args)
 	end,
 	update = function ()
+		broadcastStatus("Updating")
+		
 		downloadPrograms(true)
 		
 		os.reboot()
 	end,
 	dance = function ()
+		broadcastStatus("Dancing")
+		
 		runProgram("dance")
+	end,
+	go = function (args)
+		local n = 1
+		
+		while (n <= #args) do
+			local action, distance = args[n], 1
+			
+			if (n < #args) then
+				local value = tonumber(args[n + 1])
+				
+				if (value) then
+					distance = value
+					n = n + 1
+				end
+			end
+			
+			local handler = handlers[string.lower(action)]
+			
+			if (handler) then
+				while (distance > 0) do
+					local success, err = pcall(handler)
+					
+					if success then
+						distance = distance - 1
+					else
+						print(err)
+					end
+				end
+			end
+			
+			n = n + 1
+		end
+	end,
+	dig = function (args)
+		local direction = args[1]
+		
+		if (direction == "down") then
+			turtle.digDown()
+		elseif (direction == "up") then
+			turtle.digUp()
+		else
+			turtle.dig()
+		end
+	end,
+	ping = function ()
+		broadcastStatus("Idle")
 	end,
 	reboot = os.reboot,
 	shutdown = os.shutdown,
@@ -81,23 +179,41 @@ end
 downloadPrograms()
 
 local function main()
-	while true do
-		local id, message = rednet.receive("tCommand")
-		local args = split(message)
+	while (true) do
+		broadcastStatus("Idle")
+		
+		local id, message, protocol = rednet.receive()
+		
+		if protocol == "tCommand" then
+			local args = split(message)
 
-		local action = args[1]
-		table.remove(args, 1)
-		
-		local callback = actions[action]
-		
-		if callback then
-			local success = pcall(callback, args)
+			local action = args[1]
+			table.remove(args, 1)
 			
-			if not success then
-				print("Unable to perform action.")
+			local callback = actions[action]
+			
+			if callback then
+				local success, err = pcall(callback, args)
+				
+				if (not success) then
+					print("Unable to perform action.")
+					print(err)
+				end
 			end
+		elseif protocol == "ping" then
+			rednet.send(id, "Pong", "pong")
 		end
 	end
 end
 
-main()
+local function terminate()
+	while (true) do
+		local event = os.pullEventRaw("terminate")
+		
+		if event == "terminate" then
+			broadcastStatus("Offline")
+		end
+	end
+end
+
+parallel.waitForAny(terminate, main)
